c234d0763f8b5b644f204c04aa0f8755e5f0a52b
Mohamed Sellu <medesllu2001@gmail.com>
2020-03-05 13:49:25 +0000

    Commit to projections

--- First_Script.R
+++ First_Script.R
@@ -19,7 +19,14 @@ library(reconhub)
 install_github("reconhub/epicontacts")
 
 remotes::install_github("reconhub/linelist")
+
 remotes::install_github("reconhub/earlyR")
+
+
+
 remotes::install_github("reconhub/projections")
+
+
+
 
 
