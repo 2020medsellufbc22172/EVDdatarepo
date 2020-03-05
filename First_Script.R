d5336a8a56c7c0e8400476c99e9d1e8af0f82c1b
Mohamed Sellu <medesllu2001@gmail.com>
2020-03-05 13:29:40 +0000

    Commit to install CRAN and github packages

---
+++ First_Script.R
@@ -0,0 +1,19 @@
+install.packages("devtools")
+library(devtools)
+
+## instructions for CRAN packages
+
+install.packages("rio")
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("magrittr")
+install.packages("outbreaks")
+install.packages("incidence")
+install.packages("discrete")
+install.packages("epitrix")
+
+## instructions for github packages (latest versions)
+library(devtools)
+install_github("tidyverse/ggplot2")
+
+
